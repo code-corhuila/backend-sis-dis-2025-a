bc004809e182483671f7bcb07a21b5de4a6d59ce
Jesús Ariel González Bonilla <ariel5253@hotmail.com>
2025-03-19 19:12:52 -0500

    fetature [HU-01-dev] ✨ add structure project  API spring boot

---
+++ src/main/java/com/corhuila/backend_sis_dis_2025_a/BackendSisDis2025AApplication.java
@@ -0,0 +1,13 @@
+package com.corhuila.backend_sis_dis_2025_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSisDis2025AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSisDis2025AApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corhuila/backend_sis_dis_2025_a/BackendSisDis2025AApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corhuila.backend_sis_dis_2025_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSisDis2025AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
